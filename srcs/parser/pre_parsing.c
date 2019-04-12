3111535fb6300963f8e778301c43959c957d8064
Ywoak <ywoak83@gmail.com>
2019-04-12 03:25:10 +0200

    Desgined a solution for simple_command implementation

--- srcs/parser/pre_parsing.c
+++ srcs/parser/pre_parsing.c
@@ -33,9 +33,25 @@
 **
 ** Breakdown :
 ** 1:
-** - if token delim go on step after
-** - if LL is still existant
-** - set start as where i am
+** - Set start at first node after delim or operators if node exist
+** - Set end the node before an operator or delim (iterate from start)
+** 2:
+** - Save pointer of node before start
+** - Save pointer of node after end
+** - Set first_ptr->next to NULL
+** - Set second_ptr->previous to NULL
+** - Set start->previous to NULL
+** - Set end->next to NULL
+** 3:
+** - Create a token command
+** - Put &start in token->data (maybe make data in token type void* to be able to cast it t_type or LL) (Or create a token type LL, create another token struct to add a LL, and do with that)
+** 4:
+** - Create a node of a LL with the token in it
+** - Link next and prev the the two ptr saved at step 2
+** 5:
+** - Start step 1 again with the second_ptr as param
+** - 
 */
+
 
 
