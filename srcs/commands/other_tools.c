4e733e1e94cf8d7726bb35448cb2e6482ca8beec
Samuel BELONDRADE <sbelondr@student.42.fr>
2019-05-06 03:41:28 +0200

    add gest commands | revoir ft_pipe

---
+++ srcs/commands/other_tools.c
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   other_tools.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbelondr <sbelondr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/22 17:57:48 by sbelondr          #+#    #+#             */
+/*   Updated: 2019/05/06 01:14:38 by sbelondr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/env.h"
+
+/*
+** creer la commande depuis la struct t_arg pour pouvoir execve
+*/
+
+char		**transfer_arg(t_arg *lst_arg)
+{
+	int		i;
+	t_arg	*head;
+	char	**split;
+
+	i = 0;
+	head = lst_arg;
+	while (lst_arg)
+	{
+		i++;
+		lst_arg = lst_arg->next;
+	}
+	if (!(split = (char**)malloc(sizeof(char*) * (i + 1))))
+		return (NULL);
+	i = -1;
+	lst_arg = head;
+	while (lst_arg->value)
+	{
+		split[++i] = ft_strdup(lst_arg->value);
+		lst_arg = lst_arg->next;
+	}
+	split[++i] = NULL;
+	lst_arg = head;
+	return (split);
+}
+
+static int	path_of_commands(char ***command, char **split)
+{
+	int		i;
+	char	*str;
+	char	*dst;
+
+	i = -1;
+	while (split[++i])
+	{
+		dst = ft_strjoin(split[i], "/");
+		str = ft_strjoin(dst, (*command)[0]);
+		if (access(str, F_OK) >= 0 && access(str, X_OK) >= 0)
+		{
+			ft_strdel(&((*command)[0]));
+			(*command)[0] = ft_strdup(str);
+			ft_strdel(&str);
+			ft_strdel(&dst);
+			return (1);
+		}
+		ft_strdel(&str);
+		ft_strdel(&dst);
+	}
+	return (-1);
+}
+
+int			is_in_path(char ***command)
+{
+	int		result;
+	char	*str;
+	char	**split;
+
+	if ((!(*command)) || (!(*command)[0]))
+		return (-1);
+	if (access((*command)[0], F_OK) >= 0 && access((*command)[0], X_OK) >= 0)
+		return (1);
+	str = value_line_path(get_env(), "PATH", 0);
+	if (!str)
+		return (-1);
+	split = ft_strsplit(str, ':');
+	result = path_of_commands(&(*command), split);
+	ft_strdel(&str);
+	ft_arraydel(&split);
+	return (result);
+}
+
+int			is_builtin(char **argv, t_lex *lex)
+{
+	int		verif;
+
+	if (ft_strequ(argv[0], "env"))
+		verif = 1;//verif = builtin_env(&(*my_env), cmd);
+	else if (ft_strequ(argv[0], "set"))
+		verif = 1;//verif = builtin_set(&(*my_env), cmd);
+	else if (ft_strequ(argv[0], "setenv"))
+		verif = 1;//verif = edit_setenv(cmd, &(*my_env));
+	else if (ft_strequ(argv[0], "unsetenv"))
+		verif = 1;//verif = ft_unsetenv(argv[1], &(*my_env));
+	else if (ft_strequ(argv[0], "export"))
+		verif = 1;//verif = edit_export(argv[1], &(*my_env));
+	else if (ft_strequ(argv[0], "unset"))
+		verif = 1;//verif = ft_unset(argv[1], &(*my_env));
+	else if (ft_strequ(argv[0], "editset"))
+		verif = 1;//verif = edit_set_cmd(cmd, &(*my_env));
+	else if (ft_strequ(argv[0], "echo"))
+		verif = -1;
+	else if (ft_strequ(argv[0], "cd"))
+		verif = 1;
+	else if (ft_strequ(argv[0], "exit"))
+		verif = 2;
+	else
+		verif = -1;
+	return (verif);
+}
+
+int			gest_return(int verif)
+{
+	t_arg	*arg;
+	char	*value;
+
+	value = ft_itoa(verif);
+	arg = create_arg("?", value);
+	ft_strdel(&value);
+	verif = edit_set(arg);
+	free_arg(&arg);
+	return (verif);
+}
+
+t_env		*get_env(void)
+{
+	static t_env	*my_env;
+
+	if (my_env == NULL)
+		my_env = init_env();
+	return (my_env);
+}
---
+++ srcs/commands/parser_env.c
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbelondr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/07 10:31:02 by sbelondr          #+#    #+#             */
+/*   Updated: 2019/05/05 07:54:27 by sbelondr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/env.h"
+
+static void	copy_value(char *src, char **dst, int start, int end)
+{
+	char	*tmp;
+	char	*stock;
+
+	stock = ft_strsub(src, start, end - start);
+	tmp = ft_strjoin(*dst, stock);
+	ft_strdel(&(*dst));
+	ft_strdel(&stock);
+	(*dst) = tmp ? ft_strdup(tmp) : NULL;
+	ft_strdel(&tmp);
+}
+
+static char	*manage_var(char *str, t_env *my_env)
+{
+	char	*final;
+
+	final = value_line_path(my_env, str, 0);
+	if (!final)
+		final = ft_strdup("");
+	return (final);
+}
+
+static int	manage_home(char **dst, int index, t_env *my_env)
+{
+	char	*data;
+
+	index += 1;
+	data = manage_var("HOME", my_env);
+	ft_strdel(&(*dst));
+	(*dst) = ft_strdup(data);
+	return (index);
+}
+
+static int	modify_dst(char *tmp, t_env *my_env, char **dst)
+{
+	char	*stock;
+	char	*final;
+
+	stock = manage_var(tmp, my_env);
+	ft_strdel(&tmp);
+	final = ft_strjoin(*dst, stock);
+	ft_strdel(&(*dst));
+	if (final)
+		(*dst) = ft_strdup(final);
+	else
+		(*dst) = NULL;
+	ft_strdel(&stock);
+	ft_strdel(&final);
+	return (0);
+}
+
+static int	apply_rules(char *src, char **dst, int index, t_env *my_env)
+{
+	int		i;
+	char	*tmp;
+
+	if (src[index + 1] == '{')
+	{
+		i = ft_chr_index(src + index, '}') + index;
+		tmp = ft_strsub(src, index + 2, i - index - 2);
+	}
+	else
+	{
+		i = ft_chr_index(src + index + 1, '$');
+		if (i > -1)
+		{
+			i += index;
+			tmp = ft_strsub(src, index + 1, i - index);
+		}
+		else
+			tmp = ft_strsub(src, index + 1, ft_strlen(src));
+	}
+	modify_dst(tmp, my_env, &(*dst));
+	if (i < 0)
+		i = ft_strlen(src);
+	return (i);
+}
+
+static char	*search_var(char *src, t_env *my_env)
+{
+	int		i;
+	int		len;
+	int		last;
+	char	*dst;
+
+	i = -1;
+	last = 0;
+	len = ft_strlen(src);
+	dst = ft_strdup("");
+	while (++i < len)
+	{
+		if (src[i] == '$' || (i == 0 && src[i] == '~'))
+		{
+			if (i != last)
+				copy_value(src, &dst, last, i);
+			if (src[i] == '~')
+				i = manage_home(&dst, i, my_env) - 1;
+			else
+				i = apply_rules(src, &dst, i, my_env);
+			last = i + 1;
+		}
+	}
+	if (i != last)
+		copy_value(src, &dst, last, i);
+	return (dst);
+}
+
+void		parser_var(char ***value, t_env *my_env)
+{
+	char	*tmp;
+	int		i;
+
+	i = -1;
+	while ((*value)[++i])
+	{
+		if (ft_strchr_exist((*value)[i], '$') ||
+				ft_strchr_exist((*value)[i], '~'))
+		{
+			tmp = search_var((*value)[i], my_env);
+			ft_strdel(&((*value)[i]));
+			(*value)[i] = tmp ? ft_strdup(tmp) : NULL;
+			ft_strdel(&tmp);
+		}
+	}
+}
