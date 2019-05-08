506283b7e221f0f26e19bf5fa4542e285f02b854
Max BELLAICHE <mbellaic@e1r10p8.42.fr>
2019-05-08 03:04:28 +0200

    normed cmd_parser.c

--- srcs/parser/command/cmd_parser.c
+++ srcs/parser/command/cmd_parser.c
@@ -1,35 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_parser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbellaic <mbellaic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/08 03:02:22 by mbellaic          #+#    #+#             */
+/*   Updated: 2019/05/08 03:04:19 by mbellaic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parser.h"
 #include "sh21.h"
 #include "env.h"
 
-int				get_argc(t_lex *cursor)
+int			get_argc(t_lex *cursor)
 {
-	int len;
+	int		len;
 
 	len = 0;
 	while (cursor)
 	{
-		if(cursor->token->type == WORD || cursor->token->type == NUMBER)
+		if (cursor->token->type == WORD || cursor->token->type == NUMBER)
 			len++;
 		cursor = cursor->next;
 	}
 	return (len);
 }
-char			**get_argv(t_token *cmd_list)
+
+char		**get_argv(t_token *cmd_list)
 {
-	char **argv;
-	t_lex *cursor;
-	int len;
-	int i;
+	char	**argv;
+	t_lex	*cursor;
+	int		len;
+	int		i;
 
 	i = 0;
-	cursor = cmd_list->command; // REMEMBER TO CHANGE IT SO IT MAKES SENSE IN THE INTERPRETER
+	cursor = cmd_list->command;
 	len = get_argc(cursor);
-	if(!(argv = (char **)malloc(sizeof(char *) * (len + 1))))
+	if (!(argv = (char **)malloc(sizeof(char *) * (len + 1))))
 		return (NULL);
-	while(cursor)
+	while (cursor)
 	{
-		if(cursor->token->type == WORD || cursor->token->type == NUMBER)
+		if (cursor->token->type == WORD || cursor->token->type == NUMBER)
 			argv[i++] = ft_strdup(cursor->token->data);
 		cursor = cursor->next;
 	}
@@ -37,15 +50,15 @@ char			**get_argv(t_token *cmd_list)
 	return (argv);
 }
 
-void			files_handler(t_token *cmd_list, t_pos *pos)
+void		files_handler(t_token *cmd_list, t_pos *pos)
 {
-	t_lex *cursor;
+	t_lex	*cursor;
 
 	cursor = cmd_list->command;
-	while(cursor)
+	while (cursor)
 	{
 		if (cursor->token->type == REDIR)
-				open_file_command(cursor->redir, pos);
+			open_file_command(cursor->redir, pos);
 		cursor = cursor->next;
 	}
 }
