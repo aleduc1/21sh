7d3c20ff7267373557e2c679143eb5a4f4a16766
Antoine PRUVOST <apruvost@e2r7p9.42.fr>
2019-05-31 09:07:58 +0200

    First push alias branch

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,11 +6,7 @@
 /*   By: apruvost <apruvost@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 16:01:05 by sbelondr          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2019/05/31 04:59:06 by apruvost         ###   ########.fr       */
-=======
-/*   Updated: 2019/05/31 07:09:11 by apruvost         ###   ########.fr       */
->>>>>>> feat_builtin
+/*   Updated: 2019/05/31 08:47:03 by apruvost         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +24,13 @@ typedef struct		s_cd
 	char	*directory;
 	char	*curpath;
 }					t_cd;
+
+typedef struct		s_alias
+{
+	char	*key;
+	char	*value;
+	t_alias	*next;
+}					t_alias;
 
 int					bt_exit(char **av);
 int					bt_echo(char **av, t_redirection *r);
