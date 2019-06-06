1e9dfaaed1fe5ea34944fd1ebfbb6a6107613c3f
Nadhara <apruvost@PC-Antoine.localdomain>
2019-06-06 23:09:59 +0200

    alias builtins started, need to test replacement, need to start thinking of a hash implementation to manage aliases

--- srcs/builtins/bt_alias.c
+++ srcs/builtins/bt_alias.c
@@ -6,29 +6,49 @@
 /*   By: apruvost <apruvost@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 17:00:16 by apruvost          #+#    #+#             */
-/*   Updated: 2019/06/05 02:08:22 by apruvost         ###   ########.fr       */
+/*   Updated: 2019/06/06 13:51:53 by apruvost         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-t_alias	*bt_addalias(char **av, int i, t_alias *alias)
+#include "builtins.h"
+
+static int	bt_showalias(char **av, int i, t_alias *alias, int ret)
+{
+	t_alias	*curr;
+
+	if (!bt_alias_exists(av, i, alias))
+		return (1);
+	curr = bt_alias_get(av, i, alias);
+	ft_printf("%s=%s\n", curr->key, curr->value);
+	return (ret);
+}
+
+t_alias		*bt_addalias(char **av, int i, t_alias *alias)
 {
 	int	len;
 
-	len = ft_strchr('=', av[i]) - av[i];
+	len = av[i] - ft_strchr('=', av[i]);
+	if (bt_alias_exists(av, i, alias))
+		alias = bt_alias_replace(av, i, alias);
+	else
+		alias = bt_alias_new(av,i, alias);
+	return (alias);
 }
 
-int		bt_alias(char **av)
+int			bt_alias(char **av)
 {
 	static t_alias	*alias = NULL;
 	int				i;
+	int				ret;
 
 	i = 1;
+	ret = 0;
 	while (ac[i])
 	{
 		if (ft_strchr_exist('=', av[i]))
 			alias = bt_addalias(av, i, &(*alias));
 		else
-			bt_showalias(av, i, alias);
+			ret = bt_showalias(av, i, alias, ret);
 		++i;
 	}
 	return (0);
