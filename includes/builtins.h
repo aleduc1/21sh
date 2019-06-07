a117b05e4493ece79493f1a0e61abed9504531c4
Antoine PRUVOST <apruvost@e3r9p4.42.fr>
2019-06-07 16:39:50 +0200

    started implementing hashtable alias

--- includes/builtins.h
+++ includes/builtins.h
@@ -7,10 +7,14 @@
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 16:01:05 by sbelondr          #+#    #+#             */
 <<<<<<< HEAD
+<<<<<<< HEAD
 /*   Updated: 2019/06/02 17:36:03 by sbelondr         ###   ########.fr       */
 =======
 /*   Updated: 2019/05/31 08:47:03 by apruvost         ###   ########.fr       */
 >>>>>>> First push alias branch
+=======
+/*   Updated: 2019/06/07 16:09:26 by apruvost         ###   ########.fr       */
+>>>>>>> started implementing hashtable alias
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +39,20 @@ typedef struct		s_alias
 {
 	char	*key;
 	char	*value;
-	t_alias	*next;
 }					t_alias;
 
+<<<<<<< HEAD
 >>>>>>> First push alias branch
+=======
+typedef struct		s_ht_alias
+{
+	int		size;
+	int		count;
+	t_alias	**alias;
+}					t_ht_alias;
+
+
+>>>>>>> started implementing hashtable alias
 int					bt_exit(char **av);
 int					bt_echo(char **av, t_redirection *r);
 
